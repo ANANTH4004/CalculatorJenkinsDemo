29429503ba4ac09f750568f4defe4a990a2d0b03
ANANTH4004 <balamuruganananth@gmail.com>
2022-10-21 10:55:41 +0530

    removed mul return also

--- CalculatorJenkinsDemo/Calculator.cs
+++ CalculatorJenkinsDemo/Calculator.cs
@@ -18,7 +18,7 @@ namespace CalculatorJenkinsDemo
         }
         public int Mul(int i , int j)
         {
-            return i * j;
+            //return i * j;
         }
         public int Div(int i , int j)
         {
